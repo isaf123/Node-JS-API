1d11cee223e450a5b021cc048795e57a4ddd94be
Isa <137991239+isaf123@users.noreply.github.com>
2024-08-23 10:32:30 +0700

    port

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 const app: Application = express();
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = 9300;
 app.use(express.json());
 app.use(cors());
 app.use("/", express.static("public/profileImage"));
@@ -14,5 +14,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use("/", router);
 app.listen(PORT, () => {
-  console.log("API RUNNING", PORT);
+  console.log("API RUNNING", 9300);
 });
